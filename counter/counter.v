42911eda2e88c82da57c2b72691ba76840845a67
taki-d <taka410199@gmail.com>
2017-08-06 02:16:46 +0900

    [add]ボタンでcountupとcountdownを切り替えたw

--- counter/counter.v
+++ counter/counter.v
@@ -1,24 +1,29 @@
 
 
-module counter(clk, seg, segsel);
+module counter(clk, seg, segsel, nbtn);
 
 	output [7:0] seg;
 	output [3:0] segsel;
-
+	
+	input [3:0] nbtn;
+	wire [3:0] btn = ~nbtn;
+	
 	input clk;
 	reg [15:0] counter;
 	
 	reg [24:0] count;
 	
-	wire [15:0] next_counter;
-	
-	assign next_counter = counter + 1;
-	
 	always @(posedge clk) begin
-		count <= count + 1;
+		count = count + 1;
 		
-		if(count == 25'b1)
-			counter = next_counter;
+		if(count == 25'b1) begin
+			if(btn[0]) begin
+				counter = counter + 1;
+			end
+			else begin
+				counter = counter - 1;
+			end
+		end
 	end
 	
 	LED7Seg led7(clk, seg, segsel, counter);
